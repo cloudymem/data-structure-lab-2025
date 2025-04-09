85436670b976f78d2aa8ad0643b4893bc1d05526
cloudymem <shdbwns111142@gmail.com>
2025-04-09 09:49:21 +0900

    music service update

--- Homework/Homework/opencvtest.cpp
+++ Homework/Homework/opencvtest.cpp
@@ -3,6 +3,7 @@
 
 int main()
 {
+	std::cout << "OpenCV Version: " << CV_VERSION;
 	cv::Mat img = cv::imread("Lenna.png", cv::IMREAD_GRAYSCALE);
 	cv::imshow("lennawindow", img);
 	cv::waitKey(0);
--- MusicStreamingService/MusicStreamingService/MusicStreaming.cpp
+++ MusicStreamingService/MusicStreamingService/MusicStreaming.cpp
@@ -2,5 +2,23 @@
 
 int main()
 {
-	Music.myMusic("")
+	//music streaming service 생성
+	MusicstreamingService my_service("Spotify");
+	my_service.addMusic("Happy ending", "Mika", "Flower", 2014);
+	my_service.addMusic("Finder", "Mika", "Water", 2011);
+
+	// search music by title
+	string music_title;
+	cout << "Enter music title: ";
+	cin >> music_title;
+	Music* result = my_service.searchbyTitle(music_title);
+		if(result != NULL)
+		{
+			cout << "FOUND: " << result->getTitle() << "by" << result->getArtist() << endl;
+		}
+		else
+		{
+			cout << "NOT FOUND" << endl;
+		}
+
 }
